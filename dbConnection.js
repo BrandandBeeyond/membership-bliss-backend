0ee4b58bd4195435fdb1133de23fcd4ca700e8c5
dhiraj <dhirajmymodak22@gmail.com>
2025-11-24 18:28:50 +0530

    first commit

---
+++ dbConnection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const {MONGO_URI} = require("./utils/config");
+
+const connectTodb = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("connected to db");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = connectTodb;
---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const express = require("express");
+const cloudinary = require("cloudinary");
+const cookieparser = require("cookie-parser");
+const cors = require("cors");
+
+const {
+  PORT
+} = require("./utils/config");
+const connectTodb = require("./dbConnection");
+// const productRouter = require("./routes/product.route");
+const app = express();
+const port = PORT;
+
+connectTodb();
+
+// cloudinary.config({
+//   cloud_name: CLOUDINARY_NAME,
+//   api_key: CLOUDINARY_API_KEY,
+//   api_secret: CLOUDINARY_API_SECRET,
+// });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieparser());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(`<center><h1>Server is Started...</h1></center>`);
+});
+
+// app.use("/api/v1", productRouter);
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
