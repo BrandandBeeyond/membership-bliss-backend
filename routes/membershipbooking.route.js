cc0bf851bfbca4a2909556edf3c6a4ceca0483c9
dhiraj <dhirajmymodak22@gmail.com>
2025-12-14 10:08:00 +0530

    isauth middleware added in booking route

--- routes/membershipbooking.route.js
+++ routes/membershipbooking.route.js
@@ -1,9 +1,14 @@
 const {
   VerifyPaymentandCreateBooking,
 } = require("../controllers/membershipbooking.controller");
+const { isAuth } = require("../middlewares/middleware");
 
 const membershipbookingRouter = require("express").Router();
 
-membershipbookingRouter.post("/booking/create", VerifyPaymentandCreateBooking);
+membershipbookingRouter.post(
+  "/booking/create",
+  isAuth,
+  VerifyPaymentandCreateBooking
+);
 
 module.exports = { membershipbookingRouter };
