2337792d93701d1aa93023493379b109c5926c39
dhiraj <dhirajmymodak22@gmail.com>
2025-12-27 11:52:25 +0530

    testing

--- routes/user.route.js
+++ routes/user.route.js
@@ -9,7 +9,7 @@ const userRouter = require("express").Router();
 
 userRouter.post("/google-login", googleLogin);
 userRouter.post("/send-otp", sendOTP);
-userRouter.post("/verify-otp", VerifyOtp);
+// userRouter.post("/verify-otp", VerifyOtp);
 userRouter.post("/logout", logoutUser);
 
 module.exports = userRouter;
