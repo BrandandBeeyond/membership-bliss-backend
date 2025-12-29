298ee46b1e0212bcade9ecf8068ea8719e46150b
dhiraj <dhirajmymodak22@gmail.com>
2025-12-29 11:22:44 +0530

    is auth provided to route complete profile

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,13 +6,14 @@ const {
   verifyOTP,
   completeUserProfile,
 } = require("../controllers/user.controller");
+const { isAuth } = require("../middlewares/middleware");
 
 const userRouter = require("express").Router();
 
 userRouter.post("/google-login", googleLogin);
 userRouter.post("/send-otp", sendOTP);
 userRouter.post("/verify-otp", verifyOTP);
-userRouter.post("/complete-profile", completeUserProfile);
+userRouter.post("/complete-profile", isAuth, completeUserProfile);
 userRouter.post("/logout", logoutUser);
 
 module.exports = userRouter;
