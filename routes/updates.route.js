97903c466bf24d1a034fe0a6791b8985682e84bc
dhiraj <dhirajmymodak22@gmail.com>
2025-12-29 14:28:57 +0530

    some updates

--- routes/updates.route.js
+++ routes/updates.route.js
@@ -1,8 +1,19 @@
-const { createUpdates, getAllUpdates } = require("../controllers/update.controller");
+const upload = require("../config/multerConfig");
+const {
+  createUpdates,
+  getAllUpdates,
+} = require("../controllers/update.controller");
 
 const updatesRouter = require("express").Router();
 
-updatesRouter.post("/addupdate", createUpdates);
+updatesRouter.post(
+  "/addupdate",
+  upload.fields([
+    { name: "icon", maxCount: 1 },
+    { name: "thumbnail", maxCount: 1 },
+  ]),
+  createUpdates
+);
 updatesRouter.get("/getall", getAllUpdates);
 
 module.exports = { updatesRouter };
