96a9dbe468f17335b675af23481de2664a020c15
dhiraj <dhirajmymodak22@gmail.com>
2026-01-01 15:22:05 +0530

    user bookings populate for getting details

--- controllers/membershipbooking.controller.js
+++ controllers/membershipbooking.controller.js
@@ -136,7 +136,9 @@ const getUserBookings = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    const bookings = await MembershipBooking.find({ userId });
+    const bookings = await MembershipBooking.find({ userId })
+      .populate("membershipPlanId")
+      .sort({ createdAt: -1 });
 
     if (!bookings || bookings.length === 0) {
       return res.status(200).json({
