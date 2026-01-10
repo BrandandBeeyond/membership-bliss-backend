1141f37c8045970130f8aaeecbe324a1a6d09702
dhiraj <dhirajmymodak22@gmail.com>
2026-01-10 18:31:02 +0530

    pushing admin model

---
+++ models/Admin.model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const bcrypt = require("mongoose");
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["SUPER_ADMIN", "ADMIN", "COUNTER_STAFF"],
+      default: "ADMIN",
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+
+adminSchema.methods.comparePassword = async function(password){
+    return bcrypt.compare(password, this.password);
+}
+
